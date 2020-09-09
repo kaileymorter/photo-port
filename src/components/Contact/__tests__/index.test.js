96dba232149d5c8fbf81a4df661c955fd138f180
Spartacus <k_morter@outlook.com>
2020-09-09 15:22:48 -0600

    added Modal component and testing

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Contact from '..'
 import ContactForm from '..';
 
 afterEach(cleanup);
